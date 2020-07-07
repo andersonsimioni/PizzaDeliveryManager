24104864dc0e6c1fa0d832022986da3d8dd8a959
Willian de Souza <willian.souza.ti@gmail.com>
2020-07-06 22:13:32 -0300

    é iss

--- src/univali/andersonsimioni/Order.java
+++ src/univali/andersonsimioni/Order.java
@@ -1,28 +1,67 @@
 package univali.andersonsimioni;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Order {
+
+    private enum pizzaSize{
+        small("small"),
+        medium("medium"),
+        large("large");
+
+        private String size;
+
+        pizzaSize(String size) {
+            this.size = size;
+        }
+
+        public String getSize(){
+            return this.size;
+        }
+    }
+
     private final int OrderId;
     private final int ClientId;
     private final ArrayList<Integer> PizzasId;
+    private final ArrayList<pizzaSize> PizzasSizes;
 
-    public void AddPizza(int pizzaId){
+    public void AddPizza(int pizzaId, String size){
         if(pizzaId < 0)
             throw new IllegalArgumentException("Pizza id is smaller than zero");
 
+<<<<<<< HEAD
+        switch (size) {
+            case "small" -> PizzasSizes.add(pizzaSize.small);
+            case "medium" -> PizzasSizes.add(pizzaSize.medium);
+            case "large" -> PizzasSizes.add(pizzaSize.large);
+            default -> throw new IllegalArgumentException("Invalid pizza size, accepted: small, medium, large");
+        }
+
+=======
+>>>>>>> bb517f8d47a06a757b965d1c8c4f7917a21e2454
         PizzasId.add(pizzaId);
+    }
+
+    public ArrayList<Integer> getPizzasIds(){
+        return PizzasId;
+    }
+
+    public String getPizzaSizeByIndex(Integer index){
+        return PizzasSizes.get(index).toString();
+    }
+
+    public Integer getPizzaIdByIndex(Integer index){
+        return PizzasId.get(index);
     }
 
     public void RemovePizza(int pizzaId){
         if(pizzaId < 0)
             throw new IllegalArgumentException("Pizza id is smaller than zero");
 
-        for (Integer p:
-             PizzasId) {
+        for (Integer p: PizzasId) {
             if(p == pizzaId){
                 PizzasId.remove(p);
+                PizzasSizes.remove(p);
                 return;
             }
         }
@@ -37,5 +76,6 @@ public class Order {
         OrderId = orderId;
         ClientId = clientId;
         PizzasId = new ArrayList<Integer>();
+        PizzasSizes = new ArrayList<pizzaSize>();
     }
 }
