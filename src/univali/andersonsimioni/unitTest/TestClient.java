34cb5d107370dfa8b507dd97a13bb2c6b2fdba80
Bit <willian.souza.ti@gmail.com>
2020-07-06 21:11:10 -0300

    add test function to test client class

--- src/univali/andersonsimioni/unitTest/TestClient.java
+++ src/univali/andersonsimioni/unitTest/TestClient.java
@@ -6,4 +6,13 @@ import java.io.*;
 
 public class TestClient {
 
+    /**
+     * function to test Client class
+     */
+    public static void addClient(){
+        Client client = new Client(1, "Willian de Souza");
+        System.out.printf("Id: %d\n", client.getId());
+        System.out.printf("Name: %s", client.getName());
+    }
+
 }
